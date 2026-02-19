f6a7ed1cbce6e3e2fa95c3f68ba70e754ebc5a1b
CinaPC <cinagolshahi8@gmail.com>
2026-02-19 12:16:28 +0330

    defining the main and initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home, Movies, Series } from "./pages";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/series" element={<Series />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import Home from "./home";
+import Movies from "./movies";
+import Series from "./series";
+
+export { Home, Movies, Series };
